d7e3d99210b9471b9525f0623b7566145dc267e2
eaealvesx <100374002+eaealvesx@users.noreply.github.com>
2023-05-10 14:03:40 -0300

    Add files via upload

---
+++ heranca_polimorfismo/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿namespace heranca_polimorfismo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        FuncionarioN1 n1 = new FuncionarioN1();
+        FuncionarioN2 n2 = new FuncionarioN2();
+        FuncionarioN3 n3 = new FuncionarioN3();
+
+        n1.Nome = "Alves";
+        n1.Idade = 16;
+        n2.Nome = "Harley";
+        n2.Idade = 16;
+        n3.Nome = "Toninho";
+        n3.Idade = 76;
+        n3.Cargo = "Aposentado";
+        
+        Console.WriteLine("Nome: " + n1.Nome + " -- Idade: " + n1.Idade + " -- Part: " +  (n1.Participacao()));
+        Console.WriteLine("Nome: " + n2.Nome + " -- Idade: " + n2.Idade + " -- Part: " +  (n2.Participacao()));
+        Console.WriteLine("Nome: " + n3.Nome + " -- Idade: " + n3.Idade + " -- Part: " +  (n3.Participacao()) + " -- Cargo: " + n3.Cargo);
+    }
+}
